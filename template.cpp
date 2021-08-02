c4310a838a0c638e64d0ad2b3a5e843a9f9e1fbd
unknown <zulfikar.fauzul.akbar@gmail.com>
2021-08-03 00:14:32 +0700

    template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define fr front()
+#define pb push_back
+#define FOR(x,y,z) for(x=y;x<z;x++)
+#define pa make_pair
+#define pob pop_back()
+#define pof pop_front()
+#define pf push_front
+
+int kasus,sampai,ok,hai,data[26],i,j,k,l;
+string str;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	cin>>i;
+}
